c347b400491f8a4359e16654a9ce930e72c82b3c
gpfurlaneto <gpfurlaneto@gmail.com>
2016-07-30 15:21:08 -0300

    Melhoria tratamento de erros

--- user-app-angularjs/js/controllers/LoginCtrl.js
+++ user-app-angularjs/js/controllers/LoginCtrl.js
@@ -6,16 +6,23 @@ angular.module("listaUsuarios").controller("LoginCtrl", function($scope, loginAP
 	
 	$scope.autenticar = function (usuario) {
 		loginAPI.autenticar(usuario).success(function(data) {
-			$scope.saveUserSession(data);
-			delete $scope.usuario;
-			$scope.formLogin.$setPristine();
-			$location.path("/");
-		}).error(function(data) {
+			
 			if (Array.isArray(data)) {
 				$scope.validationErrors = $scope.getValidationErrors(data);
 			} else {
-				$.notify(data, { type: 'danger', delay: 4000} );
+				console.log('naoeharray');
+				$scope.saveUserSession(data);
+				delete $scope.usuario;
+				$scope.formLogin.$setPristine();
+				$location.path("/");
 			}
+		}).error(function(data) {
+			if (data != null) {
+				msg = data;
+			}else{
+				msg = 'Erro inesperado! Pedimos desculpa pelo inconveniente. Favor entrar em contato com o suporte.'
+			}
+			$.notify(msg, { type: 'danger', delay: 4000} );
 		});
 	};
 });
